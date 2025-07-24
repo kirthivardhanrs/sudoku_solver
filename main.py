a2f08d66904d5398c3b76d3a5948bc666a1bfcff
kirthivardhanrs <kirthivardhanrs@gmail.com>
2025-07-24 16:48:15 +0800

    basic ideas for fns

--- main.py
+++ main.py
@@ -12,4 +12,12 @@ sudoku = np.array(
     [[2, 8, 0], [0, 0, 5], [0, 7, 9]]]]
 )
 
-#sudoku[a,b,c,d] = ath row of big 3x3, bth row of big 3x3, cth row of selected small 3x3, infer for d
+def check_valid(arr):
+    return len(arr) == len(set(arr)) and len(arr) == 9
+
+def col_check(puzzle, b, d):
+    print(puzzle[:, b, :, d].flatten())
+    
+col_check(sudoku, 0, 0)
+
+#sudoku[a,b,c,d] = ath row of big 3x3, bth col of big 3x3, cth row of selected small 3x3, infer for d
